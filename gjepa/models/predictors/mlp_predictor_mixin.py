152f6570de8d5d49005a69de06bda8ec1dc356a9
Radosław Woźniak <r.wozn02@gmail.com>
2026-03-15 19:00:22 +0100

    Minor fix

--- gjepa/models/predictors/mlp_predictor_mixin.py
+++ gjepa/models/predictors/mlp_predictor_mixin.py
@@ -21,7 +21,8 @@ class MLPPredictorMixin:
             return
 
         assert activation is not None, "Activation must be provided if hidden_channels is not empty."
-        act_layer = self._act_dict[activation.lower()]()
+
+        act_cls = self._act_dict[activation.lower()]
 
         layers = []
         dims = [in_channels] + hidden_channels + [out_channels]
@@ -29,7 +30,7 @@ class MLPPredictorMixin:
         for i in range(len(dims) - 1):
             layers.append(nn.Linear(dims[i], dims[i+1]))
             if i < len(dims) - 2:
-                layers.append(act_layer)
+                layers.append(act_cls())
 
         self.mlp = nn.Sequential(*layers)
 
