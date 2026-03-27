c51153b0914c6406c3de55375a5598c0c9cf6206
Radosław Woźniak <r.wozn02@gmail.com>
2026-03-27 17:53:04 +0100

    Set 32 cpus per task

--- slurm_train_submit_scripts/submit.sh
+++ slurm_train_submit_scripts/submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=plgrid-lem-cpu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=96
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=96GB
 #SBATCH --time=24:00:00
 
